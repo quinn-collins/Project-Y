2b12ad5e69741d9041d22da7dc8dfb5f020246ce
Quinn Collins <collinsqui@gmail.com>
2018-09-04 20:15:54 -0400

    initial commit

---
+++ src/main/java/com/projecty/projecty/ProjectyApplication.java
@@ -0,0 +1,12 @@
+package com.projecty.projecty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectyApplication.class, args);
+	}
+}
